19faf1f2ac2fd78a5bcce3c13a35cb4c626ce508
LucaDiBona <lucadibonasutton@gmail.com>
2022-07-22 17:13:13 +0100

    updates

--- Compiler/Tokens/node.py
+++ Compiler/Tokens/node.py
@@ -8,6 +8,24 @@ class Node():
     def eval_(self):
         raise(NotImplementedError)
 
+class Block(Node):
+
+    def __init__(self,contents:list[Node]):
+        self.end = contents.pop()
+        self.contents = contents
+        super().__init__("Block")
+
+    def inject(self,expr:Node,pos:int):
+        #injects an expr in the block
+        self.contents.insert(pos,expr)
+
+    def eval_(self):
+        heap.enterBlock()
+        [i.eval_() for i in self.contents]
+        retVal = self.end.eval_()
+        heap.exitBlock()
+        return(retVal)
+
 class Literal(Node):
 
     def __init__(self,name:str):
@@ -31,8 +49,19 @@ class IntLiteral(Literal):
 
 class Lambda(Literal):
 
-    def __init__(self, name: str):
-        super().__init__(name)
+    def __init__(self, params:list[str], contents:Block):
+        self.params = params
+        self.contents = contents
+        super().__init__("lambda")
+
+    def eval_(self):
+        return(types_.Function_(self))
+
+    def call_(self,args:list[types_.Type_]):
+        argPairs = zip(self.params,args,strict=True)
+        for i in argPairs:
+            self.contents.inject(FunctionCall("__assign__",[i[0],i[1]]),0)
+        self.contents.eval_()
 
 class FunctionCall(Node):
 
@@ -51,6 +80,8 @@ class FunctionCall(Node):
             return(functions.BUILTINS[self.name].eval_(argVals))
 
         # lookup value
+        return(Variable(self.name).eval_().call_(argVals))
+
 
 class Variable(Node):
 
@@ -60,19 +91,6 @@ class Variable(Node):
     def eval_(self):
         return(heap.getVal(self.name))
 
-class Block(Node):
-
-    def __init__(self,contents:list[Node]):
-        self.end = contents.pop()
-        self.contents = contents
-        super().__init__("Block")
-
-    def eval_(self):
-        heap.enterBlock()
-        [i.eval_() for i in self.contents]
-        retVal = self.end.eval_()
-        heap.exitBlock()
-        return(retVal)
 
 """
 w=FunctionCall("__assign__",[StringLiteral("test"),IntLiteral("0")])
@@ -90,5 +108,27 @@ print(heap.heap) """
 #TODO allow functions as first class objects - assigning them
 #TODO allow assigning to a variable
 
-y = Block([FunctionCall("__print__",[StringLiteral("test")]),FunctionCall("__add__",[IntLiteral("2"),IntLiteral("2")])])
-print(y.eval_())
+#y = Block([FunctionCall("__print__",[StringLiteral("test")]),FunctionCall("__add__",[IntLiteral("2"),IntLiteral("2")])])
+#print(y.eval_())
+"""
+
+x = FunctionCall("__assign__",[StringLiteral("x"),StringLiteral("Hello, world!")])
+x.eval_()
+y = FunctionCall("__print__",[Variable("x")])
+y.eval_()
+
+
+ """
+
+
+x = FunctionCall("__assign__",[StringLiteral("print"),Lambda([StringLiteral("toPrint")],Block([FunctionCall("__print__",[Variable("toPrint")])]))])
+x.eval_()
+z=FunctionCall("print",[StringLiteral("Hello, world!")])
+z.eval_()
+
+
+"""
+print := (toPrint) -> {__print__(toPrint)}
+print(`Hello, world!`)
+"""
+
